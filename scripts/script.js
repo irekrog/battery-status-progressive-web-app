f9358be09e84b5484b9904f8cbe672d76073ca6e
irekrog <irekfrontend@gmail.com>
2016-09-21 12:00:19 +0200

    add ESLint

--- scripts/script.js
+++ scripts/script.js
@@ -1,93 +1,95 @@
-(function(){
+/* eslint no-console: 0 */
 
-	"use strict";
+(function () {
 
-	var Battery = {
+	'use strict';
 
-		getInfo: function() {
+	const Battery = {
+
+		getInfo() {
 
 			navigator.getBattery().then(battery => {
-		
-				let c = Battery.currentLevel(battery);
+
+				const c = Battery.currentLevel(battery);
 
 				Battery.batteryStateRender(c);
 				Battery.isCharging(battery);
 				Battery.chargingTimeChange(battery);
 				Battery.dischargingTimeChange(battery);
 
-				battery.addEventListener("levelchange", () => {
-					let c = Battery.currentLevel(battery);
+				battery.addEventListener('levelchange', () => {
+					const c = Battery.currentLevel(battery);
 					Battery.batteryStateRender(c);
 				}, false);
 
-				battery.addEventListener("chargingchange", () => {
+				battery.addEventListener('chargingchange', () => {
 					Battery.isCharging(battery);
 				}, false);
 
-				 battery.addEventListener("chargingtimechange", () => {
+				battery.addEventListener('chargingtimechange', () => {
 					Battery.chargingTimeChange(battery);
-				 }, false);
+				}, false);
 
-				 battery.addEventListener("dischargingtimechange", () => {
+				battery.addEventListener('dischargingtimechange', () => {
 					Battery.dischargingTimeChange(battery);
-				 }, false);
+				}, false);
 
 			});
 
 		},
 
-		currentLevel: function(b) {
-			let level = document.querySelector(".battery-level");
-			let tableCurrentLevel = document.querySelector(".table__cell--current-level");
+		currentLevel(b) {
+			const level = document.querySelector('.battery-level');
+			const tableCurrentLevel = document.querySelector('.table__cell--current-level');
 
-			level.textContent = ((b.level * 100).toFixed()) + "%";
-			tableCurrentLevel.textContent = ((b.level * 100).toFixed()) + "%";
+			level.textContent = ((b.level * 100).toFixed()) + '%';
+			tableCurrentLevel.textContent = ((b.level * 100).toFixed()) + '%';
 
-			level.classList.remove("mdl-spinner", "mdl-js-spinner", "is-active");
+			level.classList.remove('mdl-spinner', 'mdl-js-spinner', 'is-active');
 
 			return b.level * 100;
 		},
 
-		isCharging: function(b) {
-			let tableChargeState = document.querySelector(".table__cell--charge-state");
-			let snackbarContainer = document.querySelector('#demo-toast-example');
-		    let data = {message: b.charging ? 'Charging...' : 'Not charging'};
-		    
-		    snackbarContainer.MaterialSnackbar.showSnackbar(data);
-			
+		isCharging(b) {
+			const tableChargeState = document.querySelector('.table__cell--charge-state');
+			const snackbarContainer = document.querySelector('#demo-toast-example');
+			const data = {message: b.charging ? 'Charging...' : 'Not charging'};
+
+			snackbarContainer.MaterialSnackbar.showSnackbar(data);
+
 			tableChargeState.textContent = b.charging ? 'Charging...' : 'Not charging';
 
 			return b.charging;
 		},
 
-		chargingTimeChange: function(b) {
-			let chargeTime = document.querySelector(".table__cell--time-to-charge");
+		chargingTimeChange(b) {
+			const chargeTime = document.querySelector('.table__cell--time-to-charge');
 			chargeTime.textContent = b.chargingTime;
 		},
 
-		dischargingTimeChange: function(b) {
-			let chargeTime = document.querySelector(".table__cell--time-to-discharge");
+		dischargingTimeChange(b) {
+			const chargeTime = document.querySelector('.table__cell--time-to-discharge');
 			chargeTime.textContent = b.dischargingTime;
 		},
 
-		batteryStateRender: function(b) {
+		batteryStateRender(b) {
 			const maxHeight = 286;
-			const green = "#4CAF50";
-			const lightGreen = "#8BC34A";
-			const lime = "#CDDC39";
-			const yellow = "#FFEB3B";
-			const amber = "#FFC107";
-			const orange = "#FF9800";
-			const deepOrange = "#FF5722";
-			const red = "#FF5722";
+			const green = '#4CAF50';
+			const lightGreen = '#8BC34A';
+			const lime = '#CDDC39';
+			const yellow = '#FFEB3B';
+			const amber = '#FFC107';
+			const orange = '#FF9800';
+			const deepOrange = '#FF5722';
+			const red = '#FF5722';
 
-			let batteryFill = document.querySelector(".battery-fill");
-			let level = document.querySelector(".battery-level");
-			let currentRenderLevel = (maxHeight * b) / 100;
-			batteryFill.style.height = currentRenderLevel + "px";
+			const batteryFill = document.querySelector('.battery-fill');
+			const level = document.querySelector('.battery-level');
+			const currentRenderLevel = (maxHeight * b) / 100;
+			batteryFill.style.height = currentRenderLevel + 'px';
 
-			if (currentRenderLevel < 165) level.style.color = "#5E5E5E";
-			else level.style.color = "#FFFFFF";
+			if (currentRenderLevel < 165) level.style.color = '#5E5E5E';
+			else level.style.color = '#FFFFFF';
 
 			if (b >= 90) batteryFill.style.background = green;
 			else if (b >= 80) batteryFill.style.background = lightGreen;
@@ -99,21 +101,17 @@
 			else if (b >= 0) batteryFill.style.background = red;
 		},
 
-		init: function() {
+		init() {
 			if ('serviceWorker' in navigator) {
-  				navigator.serviceWorker.register('./sw.js', {scope: './'}).then(function(registration) {
-    				console.log('ServiceWorker registration successful with scope: ', registration.scope);
-  				}).catch(function(err) {
-    				console.log('ServiceWorker registration failed: ', err);
-  				});
+				navigator.serviceWorker.register('./sw.js', {scope: './'}).then(registration => {
+					console.log('ServiceWorker registration successful with scope: ', registration.scope);
+				}).catch(err => {
+					console.log('ServiceWorker registration failed: ', err);
+				});
 			}
-
-			Battery.getInfo();
-
+			this.getInfo();
 		}
-
-	}; 
+	};
 
 	Battery.init();
-  
 })();
--- sw.js
+++ sw.js
@@ -1,37 +1,37 @@
-var CACHE_NAME = 'v1:cache';
+/* eslint no-console: 0 */
 
-var toCache = [
-    './',
-    './styles/material.min.css',
-    './styles/main.css',
-    './scripts/material.min.js',
-    './scripts/script.js',
-    './images/battery.png',
-    './manifest.json',
-    'https://fonts.googleapis.com/icon?family=Material+Icons'
+const CACHE_NAME = 'v1:cache';
+
+const toCache = [
+	'./',
+	'./styles/material.min.css',
+	'./styles/main.css',
+	'./scripts/material.min.js',
+	'./scripts/script.js',
+	'./images/battery.png',
+	'./manifest.json',
+	'https://fonts.googleapis.com/icon?family=Material+Icons'
 ];
 
-self.addEventListener('install', function (event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(toCache);
-            })
-    );
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(cache => {
+				console.log('Opened cache');
+				return cache.addAll(toCache);
+			})
+	);
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                    // Cache hit - return response
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request);
-                }
-            )
-    );
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request)
+			.then(response => {
+				if (response) {
+					return response;
+				}
+				return fetch(event.request);
+			}
+			)
+	);
 });
