51e6f1c9c171249bb1caf4d9c4510605e1fd003b
Irek Róg <irekfrontend@gmail.com>
2018-10-26 09:30:07 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,43 @@ class BatteryStatusPWA {
     }
 
     this.getInfo();
+    
+    const btn = document.querySelector('.button');
+
+
+    let installPromptEvent;
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+      // Prevent Chrome <= 67 from automatically showing the prompt
+      console.log(event);
+      event.preventDefault();
+      // Stash the event so it can be triggered later.
+      installPromptEvent = event;
+      // installPromptEvent.prompt();
+      // Update the install UI to notify the user app can be installed
+      btn.disabled = false;
+    });
+
+    btn.addEventListener('click', () => {
+      // Update the install UI to remove the install button
+      btn.disabled = true;
+      // Show the modal add to home screen dialog
+      installPromptEvent.prompt();
+      // Wait for the user to respond to the prompt
+      installPromptEvent.userChoice.then((choice) => {
+        if (choice.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        // Clear the saved prompt since it can't be used again
+        installPromptEvent = null;
+      });
+    });
+
+    window.addEventListener('appinstalled', (evt) => {
+      console.log('installed', evt);
+    });
   }
 
   getInfo() {
